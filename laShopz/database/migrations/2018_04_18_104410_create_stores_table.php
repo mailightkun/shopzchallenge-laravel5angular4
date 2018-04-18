798a20516ff394def7c86845b7d6d0262dbed126
CHEMS ED-DINE RACHIDI <starshums@users.noreply.github.com>
2018-04-18 11:56:54 +0100

    Laravel migration

---
+++ laShopz/database/migrations/2018_04_18_104410_create_stores_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('storeid');
+            $table->string('name');
+            $table->string('address');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('like');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
